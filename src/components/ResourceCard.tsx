944edf0709f6e7e2b2f46dd602fbee128c9413e3
Cooper Ericksen <coopericksengit config --global user.email coopericksen@gmail.com>
2026-03-18 23:48:32 -0700

    Updated conditional rendering to use ternary operator

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -9,18 +9,18 @@ function ResourceCard(props: Resource) {
             <h3>{props.name}</h3>
             <p>{props.services}</p>
 
-            {(props.contact.phone != "" || props.contact.help_phone != "" || props.contact.email != "" || props.contact.website != "") && <h6 className='card-contact-header'>Contact Information</h6>}
+            {(props.contact.phone != "" || props.contact.help_phone != "" || props.contact.email != "" || props.contact.website != "") ? <h6 className='card-contact-header'>Contact Information</h6> : null}
             <div className="card-contact">
-                {props.contact.phone != "" && <p>Phone: {props.contact.phone}</p>}
-                {props.contact.help_phone != "" && <p>Help Phone: {props.contact.help_phone}</p>}
-                {props.contact.email != "" && <p>Email: {props.contact.email}</p>}
-                {props.contact.website != "" && <a href={props.contact.website} target='_blank'>Website</a>}
+                {props.contact.phone != "" ? <p>Phone: {props.contact.phone}</p> : null}
+                {props.contact.help_phone != "" ? <p>Help Phone: {props.contact.help_phone}</p> : null}
+                {props.contact.email != "" ? <p>Email: {props.contact.email}</p> : null}
+                {props.contact.website != "" ? <a href={props.contact.website} target='_blank'>Website</a> : null}
             </div>
 
-            {(props.address != "" || props.hours != "") && <h6 className='card-opinfo-header'>Operation Info</h6>}
+            {(props.address != "" || props.hours != "") ? <h6 className='card-opinfo-header'>Operation Info</h6> : null}
             <div className="card-opinfo">
-                {props.address != "" && <p>Address: {props.address}</p>}
-                {props.hours != "" && <p>Hours: {props.hours}</p>}
+                {props.address != "" ? <p>Address: {props.address}</p> : null}
+                {props.hours != "" ? <p>Hours: {props.hours}</p> : null}
             </div>
 
         </div>

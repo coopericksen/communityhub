45ec7c28e79cd7a14d454009a5cb3312cdf8f439
Cooper Ericksen <devcoopa@gmail.com>
2026-03-19 14:48:51 -0700

    Updated footer with mini nav

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,19 @@ import '../styles/Footer.css';
 function Footer() {
     return(
         <footer>
-            <h6>Washington Technology Student Association - Webmaster 2026</h6>
-            <p>Team 23027-1</p>
+            <div className='footer-left'>
+                <h6>Washington Technology Student Association - Webmaster 2026</h6>
+                <p>Team 23027-1</p>
 
-            <a href="https://github.com/coopericksen/communityhub" target='_blank'>Github Repository</a>
+                <a href="https://github.com/coopericksen/communityhub" target='_blank'>Github Repository</a>
+            </div>
+
+            <div className="footer-right">
+                <a href="/">Home</a>
+                <a href="/resources">View Resources</a>
+                <a href="/events">View Events</a>
+                <a href="/about">About</a>
+            </div>
         </footer>
     )
 }

320086dee66e03b33f307af83a098536eccb1030
Cooper Ericksen <coopericksengit config --global user.email coopericksen@gmail.com>
2026-03-19 01:55:14 -0700

    Added featured resources & events to index page

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,15 +1,33 @@
 import Nav from '../components/Nav.tsx';
 // import ElementSizes from '../components/ElementSizes.tsx';
 import Banner from '../components/Banner.tsx';
+import ResourceCard from '../components/ResourceCard.tsx';
+import EventCard from '../components/EventCard.tsx';
 import Footer from '../components/Footer.tsx';
+
+import resourcesData from '../data/resources.ts';
+import eventsData from '../data/events.ts';
 
 function IndexPage() {
     return (
         <>
             <Nav />
             <Banner page='Spotlight'/>
-            <h1>Index</h1>
             {/* <ElementSizes /> */}
+
+            <h2 className='featured-header'>Featured Resources & Events</h2>
+            <div className="card-grid-index">
+                <ResourceCard
+                    {...resourcesData[9]}
+                />
+                <EventCard
+                    {...eventsData[11]}
+                />
+                <ResourceCard
+                    {...resourcesData[3]}
+                />
+            </div>
+
             <Footer />
         </>
     )
